e2e309f7d949d954b7c45214bbcb2698cbf25a99
Farris Kamel <farris.alqalam@gmail.com>
2024-03-22 16:08:20 -0400

    Added Noice for Better UI

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,8 @@
 return {
-    { 
+    {
         'nvim-telescope/telescope.nvim', tag = '0.1.6',
         dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function ()         
+        config = function ()
             local builtin = require("telescope.builtin")
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -17,9 +17,10 @@ return {
                         require("telescope.themes").get_dropdown{
                         }
                     }
-                } 
+                }
             })
             require("telescope").load_extension("ui-select")
+            require("telescope").load_extension("noice")
         end
     },
 }
